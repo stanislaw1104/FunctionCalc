7686376ed0648d97302fa4faa7805f230f012b46
stanislaw1104 <147610845+stanislaw1104@users.noreply.github.com>
2024-03-04 13:56:30 +0700

    Add files via upload

---
+++ FunctionCalc/FunctionCalc/FunctionCalc/FunctionCalculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionCalc
+{
+    public class FunctionCalculator
+    {
+        public void Calculate(double N, int iterations, double k)
+        {
+
+            double step = 0.1;
+
+            try
+            {
+                for (double x = 0; x <= k; x += step)
+                {
+                    if (Math.Abs(x - N) < double.Epsilon)
+                    {
+                        throw new ArgumentException("Ошибка! Деление на ноль невозможно! ");
+                    }
+                    if (k < x)
+                    {
+                        throw new ArgumentException("Оширбка! Выход за пределы диапазона! ");
+                    }
+
+                    double result = 1 / (x - N);
+                    Console.WriteLine($"f({x}) = {result}");
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Исключение поймано!  \n {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Исключение! \n {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Расчет завершен.");
+            }
+        }
+    }
+
+
+}
---
+++ FunctionCalc/FunctionCalc/Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using FunctionCalc;
+internal class Program
+{
+        static void Main(string[] args)
+        {
+            bool f = true;
+            while (f)
+            {
+                Console.WriteLine("введите N");
+                double N = double.Parse(Console.ReadLine());
+                Console.WriteLine("введите K");
+                double K = double.Parse(Console.ReadLine());
+            Console.WriteLine("введите iterations");
+            int iterations = int.Parse(Console.ReadLine());
+                FunctionCalculator functionCalculator = new FunctionCalculator();
+                functionCalculator.Calculate(N, iterations, K);
+
+                Console.WriteLine("Хотите повдорить? (да-1; нет-0)");
+                int q = int.Parse(Console.ReadLine());
+
+                if (q==1)
+                {
+                    f = true;
+                }
+                else f= false;
+            }
+
+        }
+}
